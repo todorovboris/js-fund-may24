e663ea29f7c6178df8b3e69ff605176211eee0c6
Boris Todorov <boristodorov@Boriss-MacBook-Air.local>
2024-06-05 22:15:32 +0300

    Lab 03 - Arrays created and first 2 tasks added

---
+++ 03-Arrays-Lab/01-sumFirstAndLastElement.js
@@ -0,0 +1,12 @@
+function exe03(numbers){
+    let firstNum = numbers[0];
+    let lastNum = numbers[numbers.length - 1];
+    let sum = firstNum + lastNum;
+
+    console.log(sum);
+
+}
+
+exe03([20, 30, 40]);
+exe03([10, 17, 22, 33]);
+exe03([11, 58, 69]);
