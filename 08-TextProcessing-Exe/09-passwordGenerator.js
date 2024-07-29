058699d02e1f4bb0c8affed1f608a26c9b52bea6
Boris Todorov <boristodorov@Boriss-MacBook-Air.local>
2024-07-29 22:41:35 +0300

    regex lab added

--- 08-TextProcessing-Exe/09-passwordGenerator.js
+++ 08-TextProcessing-Exe/09-passwordGenerator.js
@@ -1,16 +1,20 @@
 function solve(arr) {
-    const wordForReplace = arr[2];
-    const concatenatedText = arr[0] + arr[1];
+    let wordForReplace = arr[2];
+    let concatenatedText = arr[0] + arr[1];
 
-    const vowelRegex = /^[aeiouAEIOU]$/;
+    let vowelRegex = /^[aeiouAEIOU]$/;
     let concatenatedTextArr = concatenatedText.split('');
     let replacementIndex = 0;
 
     for (let i = 0; i < concatenatedText.length; i++) {
         if (vowelRegex.test(concatenatedTextArr[i])) {
-            let replacementChar = wordForReplace[replacementIndex % wordForReplace.length].toUpperCase(); //итерира през всяка буква от 3тата дума на входния масив и я запазва в отделна променлива, като ГЛАВНА
-            concatenatedTextArr[i] = replacementChar;
+            let replacementChar = wordForReplace[replacementIndex]; //итерира през всяка буква от 3тата дума на входния масив и я запазва в отделна променлива, като ГЛАВНА
+            concatenatedTextArr[i] = replacementChar.toUpperCase();
             replacementIndex++;
+
+            if (replacementIndex === wordForReplace.length) {
+                replacementIndex = 0;
+            }
         } else {
             concatenatedTextArr[i] = concatenatedTextArr[i].toLowerCase();
         }
