5cf8654d7092b8dc683aed310d9bf702aa134899
Boris Todorov <boristodorov@Boriss-MacBook-Air.local>
2024-08-05 18:36:19 +0300

    some updates

--- 99-FinalExam/01-solve.js
+++ 99-FinalExam/01-solve.js
@@ -1,6 +1,54 @@
-function solve() {
-    //
+function solve(arr) {
+    let word = arr.shift();
+
+    let command = arr.shift();
+    while (command !== 'Abracadabra') {
+        let commandsArr = command.split(' ');
+        let action = commandsArr[0];
+
+        if (action === 'Abjuration') {
+            word = word.toUpperCase();
+            console.log(word);
+        } else if (action === 'Necromancy') {
+            word = word.toLowerCase();
+            console.log(word);
+        } else if (action === 'Illusion') {
+            let index = Number(commandsArr[1]);
+            let letter = commandsArr[2];
+
+            if (index >= word.length) {
+                console.log('The spell was too weak.');
+            } else {
+                let replacement1 = word.substring(0, index);
+                let replacement2 = word.substring(index + 1);
+
+                word = replacement1 + letter + replacement2;
+                console.log('Done!');
+            }
+        } else if (action === 'Divination') {
+            let firstSubstr = commandsArr[1];
+            let secondSubstr = commandsArr[2];
+
+            if (word.includes(firstSubstr)) {
+                word = word.split(firstSubstr).join(secondSubstr);
+                console.log(word);
+            }
+        } else if (action === 'Alteration') {
+            let substr = commandsArr[1];
+            if (word.includes(substr)) {
+                word = word.replace(substr, '');
+                console.log(word);
+            }
+        } else {
+            console.log('The spell did not work!');
+        }
+
+        command = arr.shift();
+    }
+
+    // console.log(word);
 }
 
-solve();
-solve();
+solve(['A7ci0', 'Illusion 1 c', 'Illusion 4 o', 'Abjuration', 'Divination CC DD', 'Abracadabra']);
+// solve(['TR1GG3R', 'Necromancy', 'Illusion 8 m', 'Illusion 9 n', 'Abracadabra']);
+// solve(['SwordMaster', 'Target Target Target', 'Abjuration', 'Necromancy', 'Alteration master', 'Abracadabra']);
--- 99-FinalExam/02-solve.js
+++ 99-FinalExam/02-solve.js
@@ -1,6 +1,45 @@
-function solve() {
-    //
+// function solve(messages) {
+//     const input = messages[0];
+//     const regex = /([*^]+)(?<name>[a-zA-Z\s]{6,})\1[^a-zA-Z0-9]*\+([-+]?\d{1,3}(?:\.\d+)?),([-+]?\d{1,3}(?:\.\d+)?)\+/g;
+//     let match;
+//     let foundAny = false;
+
+//     // let match = input.match(regex);
+//     // console.log(match);
+
+//     while ((match = regex.exec(input)) !== null) {
+//         const artifactName = match[2].trim();
+//         const coordinates = `${match[3]},${match[4]}`;
+
+//         console.log(`Found ${artifactName} at coordinates ${coordinates}.`);
+//         foundAny = true;
+//     }
+
+//     if (!foundAny) {
+//         console.log('No valid artifacts found.');
+//     }
+// }
+
+// // Test the function with the given input
+// solve(['*Lost Crown*+++34.807,-40.479+++^Ancient Sword^++++48.8566,2.3522++++*Golden Statue*!!!+12.492,99.901+!!!*Lost Crown*+++34.807,-40.479+']);
+
+function findArtifacts(messages) {
+    const input = messages[0];
+    const artifactRegex = /([*^]+)(?<name>[a-zA-Z\s]{6,})\1[^a-zA-Z0-9]*\+([-+]?\d{1,3}(?:\.\d+)?),([-+]?\d{1,3}(?:\.\d+)?)\+/g;
+    let match;
+    let foundAny = false;
+
+    while ((match = artifactRegex.exec(input)) !== null) {
+        const artifactName = match[2].trim();
+        const coordinates = `${match[3]},${match[4]}`;
+
+        console.log(`Found ${artifactName} at coordinates ${coordinates}.`);
+        foundAny = true;
+    }
+
+    if (!foundAny) {
+        console.log('No valid artifacts found.');
+    }
 }
 
-solve();
-solve();
+findArtifacts(['*Lost Crown*+++34.807,-40.479+++^Ancient Sword^++++48.8566,2.3522++++*Golden Statue*!!!+12.492,99.901+!!!*Lost Crown*+++34.807,-40.479+']);
